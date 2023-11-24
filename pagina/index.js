ef674233d258f1ab0bd0bf25204a0992049c9f8f
Litzy Diaz <108082744+LitzyDiaz0@users.noreply.github.com>
2023-11-23 18:44:43 -0600

    Add files via upload

---
+++ pagina/index.js
@@ -0,0 +1,7 @@
+function validar() {
+   //  alert($("#user").val());
+    var user = document.getElementById("user");
+    var password = document.getElementById("password");
+
+    alert("Tu usuario es: "+user.value+"   Contraseña: " +password.value);
+}
